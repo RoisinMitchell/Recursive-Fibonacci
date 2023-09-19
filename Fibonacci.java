a0a0faacaf930957bba26b5432e1433cc136488b
RoisinMitchell <mitchell.roisin@gmail.com>
2023-09-19 13:30:57 +0100

    commit

--- Fibonacci.java
+++ Fibonacci.java
@@ -1,2 +1,24 @@
+import java.util.ArrayList;
+
 public class Fibonacci {
+    public static void main(String[] args){
+        int x = (int) (Math.random() * 10);
+        Fibonacci myFib = new Fibonacci();
+        String sequence = myFib.fib(x);
+        System.out.println("Number = " + x + ": " +sequence);
+    }
+    public static ArrayList<Integer> fib(int x){
+        ArrayList<Integer> sequence = new ArrayList<>();
+        sequence.add(0);
+        sequence.add(1);
+
+        if(x!= 0){
+              sequence.add(fib(x-1)  sequence.add(x));
+        }else{
+            sequence = sequence + 1;
+        }
+        return sequence;
+    }
+
+
 }
